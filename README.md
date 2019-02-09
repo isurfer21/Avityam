67ccfa1ed8d07fe9545ff31a8884d419c5d25242
Abhishek Kumar <akbittu@gmail.com>
2019-02-09 18:43:43 +0530

    Added secure micro server

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Avityam
-A secured server based on HTTPS over local machine
+A secured micro webserver based on HTTPS over local machine
+
+### Setup
+```
+git clone https://github.com/isurfer21/Avityam.git
+```
+
+### Usage
+```
+$ node server.js -h
+Avityam webserver (Version 1.0)
+Copyright (c) 2019 Abhishek Kumar.
+Licensed under the MIT License.
+
+Options:
+  -h, --help               display help information
+  -u, --host[=127.0.0.1]   set host IP or server address
+  -p, --port[=8080]        set custom port number
+  -d, --docpath            set document directory's path
+  -k, --key                set SSL key's path
+  -c, --cert               set SSL certificate's path
+	
+```
+
+#### Examples
+To specifiy custom host IP and port
+```bash
+$ node server.js -u=192.168.0.1 -p=9000
+```
+To specifiy custom host IP, port and docpath
+```bash
+$ node server.js -d=/Users/abhishekkumar/webapp/ -u=192.168.0.1 -p=9000
+```
+To specifiy custom docpath and SSL certificate with key
+```bash
+$ node server.js \
+ -d=/Users/abhishekkumar/webapp/ \
+ -k=/Users/abhishekkumar/mkcert/rootCA-key.pem \
+ -c=/Users/abhishekkumar/mkcert/rootCA.pem
+```
+
+### Dependency
+You can use [mkcert](https://github.com/FiloSottile/mkcert) is a simple tool for making locally-trusted development certificates.
+
---
+++ server.js
@@ -0,0 +1,67 @@
+var https = require('https'),
+    fs = require('fs'),
+    path = require('path'),
+    minimist = require('minimist'),
+    express = require('express');
+
+console.log(`Avityam webserver (Version 1.0)
+Copyright (c) 2019 Abhishek Kumar.
+Licensed under the MIT License.
+`);
+
+var argv = minimist(process.argv.slice(2));
+// console.dir(argv);
+
+var parseArgv = function(shortArg, longArg, typeArg, defaultVal) {
+    let argVal;
+    if (!!argv[shortArg] && typeof argv[shortArg] == typeArg) {
+        argVal = argv[shortArg];
+    } else if (!!argv[longArg] && typeof argv[longArg] == typeArg) {
+        argVal = argv[longArg];
+    } else {
+        argVal = defaultVal;
+    }
+    return argVal;
+}
+
+var help = parseArgv('help', 'h', 'boolean', false),
+    host = parseArgv('host', 'u', 'string', '127.0.0.1'),
+    port = parseArgv('port', 'p', 'number', '8080'),
+    docpath = parseArgv('docpath', 'd', 'string', __dirname),
+    key = parseArgv('key', 'k', 'string', ''),
+    cert = parseArgv('cert', 'c', 'string', '');
+
+if (help) {
+    console.log(`Options:
+  -h, --help               display help information
+  -u, --host[=127.0.0.1]   set host IP or server address
+  -p, --port[=8080]        set custom port number
+  -d, --docpath            set document directory's path
+  -k, --key                set SSL key's path
+  -c, --cert               set SSL certificate's path
+	`);
+} else {
+    var app = express();
+    console.log('Server root is', docpath);
+    if (!!key && !!cert) {
+        if (fs.existsSync(key)) {
+            if (fs.existsSync(cert)) {
+                let options = {
+                    key: fs.readFileSync(key),
+                    cert: fs.readFileSync(cert)
+                };
+                console.log('Using SSL, \n Key at ' + key + '\n Certificate at ' + cert);
+                https.createServer(options, app).listen(port);
+                app.use('/', express.static(docpath));
+                console.log('Listening at https://' + host + ':' + port);
+            } else {
+            	console.log('Invalid "cert" file path!');
+            }
+        } else {
+        	console.log('Invalid "key" file path!');
+        }
+    } else {
+        app.use('/', express.static(docpath));
+        app.listen(port, host, () => console.log('Server listening at http://' + host + ':' + port));
+    }
+}
